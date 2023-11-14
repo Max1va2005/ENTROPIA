212f5af32ada4769e466e3300c8e0be6b771492f
Max Ivashchenko <maximivashchenko05@gmail.com>
2023-11-14 18:33:36 +0200

    Add project files.

---
+++ 2_task/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ймовірності та тривалості
+        double[] probabilities = { 0.37, 0.18, 0.06, 0.3 };
+        double[] durations = { 5, 14, 8, 3 };
+
+        // Ентропія
+        double entropy = -Sum(p => p * Math.Log2(p), probabilities);
+
+        // Продуктивність
+        double productivity = entropy / Sum((p, t) => p * t, probabilities, durations);
+
+        // Надмірність
+        double redundancy = 1-(entropy / Math.Log2(durations.Length));
+
+        Console.WriteLine("Ентропія: " + entropy);
+        Console.WriteLine("Продуктивність: " + productivity);
+        Console.WriteLine("Надмірність: " + redundancy);
+    }
+
+    // Функція для обчислення суми з використанням лямбда-виразу
+    static double Sum(Func<double, double> func, params double[] values)
+    {
+        double result = 0;
+        foreach (var value in values)
+        {
+            result += func(value);
+        }
+        return result;
+    }
+
+    // Функція для обчислення суми з використанням лямбда-виразу з двома параметрами
+    static double Sum(Func<double, double, double> func, double[] values1, double[] values2)
+    {
+        double result = 0;
+        for (int i = 0; i < values1.Length; i++)
+        {
+            result += func(values1[i], values2[i]);
+        }
+        return result;
+    }
+}
---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Ймовірності
+        double[][] p_y_given_x = {
+            new double[] {0.53, 0.34, 0.13},  // p(y1/x1), p(y2/x1), p(y3/x1)
+            new double[] {0.33, 0.14, 0.53}   // p(y1/x2), p(y2/x2), p(y3/x2)
+        };
+        double[] p_y = { 0.368, 0.178, 0.454 };  // p(y1), p(y2), p(y3)
+
+        // Алфавіти
+        int M = p_y_given_x.Length;
+        int N = p_y.Length;
+
+        // Ентропія системи
+        double entropy_system = -Sum(p_x => Sum(p_y => p_y * Math.Log2(p_y), p_y_given_x[p_x]), M);
+
+        // Повна взаємна інформація
+        double mutual_information = Sum(p_x => Sum(p_y => p_y_given_x[p_x][p_y] * Math.Log2(p_y_given_x[p_x][p_y] / (p_y * p_y_given_x[p_x].Sum())), N), M);
+
+        // Надмірність
+        double redundancy = entropy_system / mutual_information;
+
+        Console.WriteLine("Ентропія системи (H(X, Y)): " + entropy_system);
+        Console.WriteLine("Повна взаємна інформація (I(X, Y)): " + mutual_information);
+        Console.WriteLine("Надмірність (R(X|Y)): " + redundancy);
+    }
+
+    private static double Sum(Func<int, double> value, double[] doubles)
+    {
+        throw new NotImplementedException();
+    }
+
+    // Функція для обчислення суми з використанням лямбда-виразу
+    static double Sum(Func<int, double> func, int max)
+    {
+        double result = 0;
+        for (int i = 0; i < max; i++)
+        {
+            result += func(i);
+        }
+        return result;
+    }
+}
